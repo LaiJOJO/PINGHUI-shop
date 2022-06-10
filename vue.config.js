09ae80e35cba04cc14d52af42d1e5334728be371
JOJO <2485706183@qq.com>
2022-06-10 20:26:21 +0800

    临时部署

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  outputDir:'dist',
+  publicPath: process.env.NODE_ENV === 'production' ? '/pinhui-shop-web/' : '/',
   transpileDependencies: true,
   lintOnSave:false,
   devServer:{
