b0a79ef92b0ab4f592d4c6bddd7a9f944ddb42ec
JOJO <2485706183@qq.com>
2022-06-10 21:16:43 +0800

    ..

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  assetsPublicPath:'./',
   outputDir:'dist',
   publicPath: process.env.NODE_ENV === 'production' ? '/pinhui-shop-web/' : '/',
   transpileDependencies: true,
